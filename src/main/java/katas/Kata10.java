26ae13ea7e802e4ae6d8875b36c90603f2918c09
Brayan Silva <brayan_silva82151@elpoli.edu.co>
2021-12-09 12:34:23 -0500

    Kata 10

--- src/main/java/katas/Kata10.java
+++ src/main/java/katas/Kata10.java
@@ -8,16 +8,15 @@ import util.DataUtil;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Create a datastructure from the given data:
-
     We have 2 arrays each containing lists, and videos respectively.
     Each video has a listId field indicating its parent list.
     We want to build an array of list objects, each with a name and a videos array.
     The videos array will contain the video's id and title.
     In other words we want to build the following structure:
-
     [
         {
             "name": "New Releases",
@@ -46,7 +45,6 @@ import java.util.Map;
             ]
         }
     ]
-
     DataSource: DataUtil.getLists(), DataUtil.getVideos()
     Output: the given datastructure
 */
@@ -55,9 +53,12 @@ public class Kata10 {
         List<Map> lists = DataUtil.getLists();
         List<Map> videos = DataUtil.getVideos();
 
-        return ImmutableList.of(ImmutableMap.of("name", "someName", "videos", ImmutableList.of(
-                ImmutableMap.of("id", 5, "title", "The Chamber"),
-                ImmutableMap.of("id", 3, "title", "Fracture")
-        )));
+        return (lists.stream().map(list -> ImmutableMap.of("name",list.get("name"),"videos"
+                ,videos.stream().filter((video)->(video.get("listId").equals(list.get("id"))))
+                        .collect(Collectors.toList()))).collect(Collectors.toList()));
+         /* return ImmutableList.of(ImmutableMap.of("name", "someName", "videos", ImmutableList.of(
+                ImmutableMap.of("id", 5434364, "title", "The Chamber"),
+                ImmutableMap.of("id", 65456475, "title", "Fracture")
+        ))); */
     }
 }
